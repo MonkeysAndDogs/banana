2c2d282b236bee3f6e3d7907afdd00eb20fec7d8
曲士界 <qusj@tydic.com>
2017-10-01 15:40:18 +0800

    added idea config

---
+++ src/main/java/zoo/fruit/banana/wechat/WechatApplication.java
@@ -0,0 +1,12 @@
+package zoo.fruit.banana.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatApplication.class, args);
+	}
+}
