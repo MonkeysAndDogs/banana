83102843fbcb7ec818e28da868621a078fb3c287
曲士界 <qusj@tydic.com>
2017-10-02 01:08:13 +0800

    added home

--- tree/src/main/java/zoo/monkeys/banana/tree/TreeApplication.java
+++ tree/src/main/java/zoo/monkeys/banana/tree/TreeApplication.java
@@ -2,11 +2,19 @@ package zoo.monkeys.banana.tree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TreeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TreeApplication.class, args);
-	}
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeApplication.class, args);
+    }
 }
