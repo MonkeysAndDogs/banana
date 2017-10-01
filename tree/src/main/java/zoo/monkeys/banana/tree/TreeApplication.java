a22108765dab2a4bae92a474bf5d28223bcbae39
曲士界 <qusj@tydic.com>
2017-10-02 02:24:21 +0800

    docker

--- tree/src/main/java/zoo/monkeys/banana/tree/TreeApplication.java
+++ tree/src/main/java/zoo/monkeys/banana/tree/TreeApplication.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TreeApplication {
 
+    private int count;
+
     @RequestMapping("/")
     public String home() {
+        System.out.println("count:" + ++count);
         return "Hello Docker World";
     }
 
